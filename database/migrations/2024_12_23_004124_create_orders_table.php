5df61e5c67721d25fdf5b3dfd902087039563ed4
Richard Kweku Aikins <richardkns7@gmail.com>
2024-12-25 16:32:09 +0000

    Add 2024_12_23_004124_create_orders_table.php

---
+++ database/migrations/2024_12_23_004124_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['pending', 'accepted', 'paid', 'delivered']);
+            $table->bigInteger('user_id')->default(1);
+            $table->string('comment')->nullable();
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
