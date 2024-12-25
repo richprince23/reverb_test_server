66a57960b97aa1f0fec35a5d0de73bbc1a251b8b
Richard Kweku Aikins <richardkns7@gmail.com>
2024-12-25 16:32:10 +0000

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 import './bootstrap';
+
+
+const eventList = document.querySelector('.event-list');
+
+const event = document.createElement('div');
+event.classList.add('event');
+event.innerHTML = `
+        <div class="flex flex-col gap-2">
+            <div class="flex justify-between items-center">
+                <span class="text-sm text-blue-700">Hellllllo</span>
+            </div>
+        </div>`;
+
+
+eventList.appendChild(event);
+
+
+Echo.channel('test-channel').listen('TestEvent', (e) => {
+    const event = document.createElement('div');
+    event.classList.add('event');
+    event.innerHTML = `
+            <div class="flex flex-col gap-2">
+                <div class="flex justify-between items-center">
+                    <span class="text-sm text-blue-700">Another</span>
+                </div>
+            </div>`;
+
+
+    eventList.appendChild(event);
+    console.log(e);
+});
